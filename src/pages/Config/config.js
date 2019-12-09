388b0364432956e353fd24f7456c8559137ac789
Martin Klauer <mdklauer@gmail.com>
2019-12-09 15:07:36 +0100

    delete config.js

--- src/pages/Config/config.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase';
-
-export default firebase.initializeApp({
-  apiKey: "AIzaSyAZhYVFTXLNrcQBi7qK9Gr-ZbGNHfpAtwE",
-  authDomain: "testfirebase-ca33c.firebaseapp.com",
-});
